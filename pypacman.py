c8594c0b2c7dbc94ec99be657b894e94e3246030
Johannes Hausmann <johannes.hausmann@th-bingen.de>
2020-01-14 11:52:58 +0100

    added pacstrap class

--- pypacman.py
+++ pypacman.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
+import logging
 
-class pacman:
+class pacman(object):
 
     def __init__(self):
         self.path="/usr/bin/pacman"
         self.config="/etc/pacman.conf"
         
         try:
-            subprocess.check_call([self.path,'-V'])
+            subprocess.check_call([self.path,'-V'],stdout=subprocess.DEVNULL)
         except subprocess.CalledProcessError as e:
             print (e)
 
@@ -45,22 +47,22 @@ class pacman:
     
     def list_installed(self):
         try:
-            installed = subprocess.check_output([self.path,'-Qq'],encoding="utf-8")
-            return(installed.rstrip('\n').split('\n'))
+            installed = subprocess.getoutput([self.path + ' -Qq'])
+            return(installed.split('\n'))
         except subprocess.CalledProcessError as e:
             print (e)
     
     def list_explicit_installed(self):
         try:
-            installed = subprocess.check_output([self.path,'-Qenq'],encoding="utf-8")
-            return(installed.rstrip('\n').split('\n'))
+            installed = subprocess.getoutput([self.path + ' -Qenq'])
+            return(installed.split('\n'))
         except subprocess.CalledProcessError as e:
             print (e)
     
     def autoremove(self):
-        packages = subprocess.check_output(['pacman','-Qdtq'],encoding="utf-8")
+        packages = subprocess.getoutput([self.path + ' -Qdtq'])
         if packages:
-            self.remove(packages.rstrip('\n').split('\n'))
+            self.remove(packages.split('\n'))
         else:
             print ('No packages to remove')
     
@@ -74,5 +76,27 @@ class pacman:
         except:
             print('An error occurred')
 
-			
-	
+class pacstrap:
+    def __init__(self):
+        self.path="/usr/bin/pacstrap"
+        self.logger = logging.getlogger('pacstrap-logger')
+        self.root = "/mnt"
+        try:
+            subprocess.check_call([self.path,'-h'],stdout=subprocess.DEVNULL)
+        except subprocess.CalledProcessError as e:
+            print (e)
+        
+    def pacstrap(self,packages:list):
+        try:
+            subprocess.call([self.path,self.root] + packages)
+        except subprocess.CalledProcessError as e:
+            print (e)
+    
+    def pacstrap_auto(self):
+        packages=['base','base-devel','linux','nano','dhcpcd','wifi-menu','linux-firmware',
+                  'device-mapper', 'inetutils', 'man-db', 'sysfsutils', 'texinfo', 'usbutils',
+                  'xfsprogs','bash-completion','wpa_supplicant','dialog','netctl']
+        self.pacstrap(packages)
+
+
+
